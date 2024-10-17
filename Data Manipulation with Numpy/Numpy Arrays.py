c29d8b4dcbb8f2b48e6d6e926edf9ea3d0cdc0aa
Akshit Gaur <akshitgaur997@gmail.com>
2024-10-17 12:52:40 -0700

    Create Numpy Arrays.py

---
+++ Data Manipulation with Numpy/Numpy Arrays.py
@@ -0,0 +1,9 @@
+import numpy as np 
+
+#Creating a numpy arr of type Float
+arr = np.array([[0,1,2],[3,4,5]], dtype = np.float32)
+print(repr(arr))
+
+#Upcasting: String > Float > Integer 
+arr = np.array([0,0.1,2])
+
